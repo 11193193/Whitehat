dee1aafb2cb15d09100655c1d1223851a75e90d0
11193193 <11193193@college.wlc.ac.uk>
2025-02-06 11:56:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ from azure.ai.inference.models import UserMessage
 import requests
 from utils.parser import Parser
 from utils.AI import ChatClient
+from fastapi import FastAPI, Request
 
-url = 'https://uk.linkedin.com/in/joe-gannon'
-apikey = 'a078893d0e089678f25e590c2533dbf10cb4817b'
-params = {
-    'url': url,
-    'apikey': apikey,
-    'js_render': 'true',
-    'premium_proxy': 'true',
-}
-"""
-response = requests.get('https://api.zenrows.com/v1/', params=params)
+app = FastAPI()
 
-print(response.text)"""
-if __name__ == "__main__":
+@app.get("/")
+async def root(link: str):
+    apikey = 'a078893d0e089678f25e590c2533dbf10cb4817b'
+    params = {
+        'url': link.strip(" "),
+        'apikey': apikey,
+        'js_render': 'true',
+        'premium_proxy': 'true',
+    }
+    
+    response = requests.get('https://api.zenrows.com/v1/', params=params)
+
+    if response.status_code != 200:
+        return {'status': False, 'content': f'zenrows scraper returned {response.status_code}'}
+        
+    return {
+        "status":True
+        }
+
+"""if __name__ == "__main__":
     parser = Parser("html.html")
     personalData = parser.getData()
     AI = ChatClient()
     aiResponse = AI.getCompletion(userMessage=str(personalData), product="A backup raid system")
 
-    print(aiResponse)
+    print(aiResponse)"""
